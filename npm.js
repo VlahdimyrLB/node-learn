34e72b2c0773b035d2722b78472da6fe7ed9d2dc
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-04-07 22:36:42 +0800

    Finished npm

---
+++ npm.js
@@ -0,0 +1,30 @@
+// npm - global command, comes with node by default
+// npm --version - to see the npm version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/packages)
+// npm init -y
+
+// add -D for dev dependecies
+// this dev dependencies are dependecie only use in development but not in production
+// examples. nodemon
+
+//to delete package
+// npm uninstall <packageName>
+
+// NPX command
+// using this we dont need to install a package globally
+// npx is used for executing command-line tools that are part of a package without the need for a global or local installation
+
+// dependcy versioning major.minor.patch
+// major - break changes
+// minor - backwards compatible, depracated(old functionality) but operational, large internal refactor
+// patch - bug fixes
+
+console.log("NICESU");
